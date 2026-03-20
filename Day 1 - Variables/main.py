57819c4f677a58977eb0a05f875ff3abb3b03199
Ivan Justin Ladera <ivanjustinladera0722@gmail.com>
2026-03-20 23:53:24 +0800

    Update main.py

--- Day 1 - Variables/main.py
+++ Day 1 - Variables/main.py
@@ -1,15 +1,12 @@
 # Day 1 - Variables and Data Types
 
-## What I Learned
-- A variable is used to store data
-- Python has different data types like int, float, string, and boolean
-- input() is used to get user input
-- int() and float() are used to convert input into numbers
-
-## Example Code
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
+height = float(input("Enter your height (in meters): "))
+is_student = True
 
-## Reflection
-At first, I was confused about data types and input conversion,
-but after trying the code, I understood how values are stored and displayed.
+print("\n--- USER INFORMATION ---")
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
