ece087f07a5ab7ba748ee7d717db8f7b719aec9b
Ivan Justin Ladera <ivanjustinladera0722@gmail.com>
2026-03-21 01:02:17 +0800

    exercise 2

---
+++ Day 1- Variables/exercie2.py
@@ -0,0 +1,6 @@
+first_number = float(input("first number: "))
+second_number = float(input("second number: "))
+
+sum = print(first_number + second_number)
+difference = print(first_number - second_number)
+product = print(first_number * second_number)
