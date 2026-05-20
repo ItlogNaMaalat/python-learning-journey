c47637dba2e899a252bef7f6cba2c270f98966b3
Ivan Justin Ladera <ivanjustinladera0722@gmail.com>
2026-05-20 10:42:18 +0800

    added exercise1.py

---
+++ Day 6 - List/Exercises/exercise1.py
@@ -0,0 +1,18 @@
+# Exercise 1 — Favorite Movies List
+
+# Create a program that:
+#   creates a list of 5 favorite movies
+#   prints the first movie
+#   prints the last movie
+#   prints the total number of movies
+
+# HINT
+#   movies[0]
+#   movies[-1]
+#   len(movies)
+
+fav_movies = ["Movie 1", "Movie 2", "Movie 3", "Movie 4", "Movie 5"]
+
+print(fav_movies[0])
+print(fav_movies[-1])
+print(len(fav_movies))
